1ec4ee6108808c1b0d4072057b0e4950a52a5ea9
thanhbin <thanhbin15051989@gmail.com>
2013-09-30 19:55:27 +0700

    thanhbin

---
+++ DemoGit01/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoGit01
+//
+//  Created by Techmaster on 9/30/13.
+//  Copyright (c) 2013 Techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoGit01/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoGit01
+//
+//  Created by Techmaster on 9/30/13.
+//  Copyright (c) 2013 Techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
